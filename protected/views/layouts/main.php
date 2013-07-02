6b9955215258c61b835b3a10d57a33ce140d1af9
Kunal Kamble <kunalkamble@Kunal-Kambles-MacBook-Pro-2.local>
2013-07-02 07:58:05 -0400

    Frontend - Basic changes to home page and navigation links.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,8 +30,9 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'User', 'url'=>array('/user', 'view'=>'user')),
+				array('label'=>'User Group', 'url'=>array('/userGroup', 'view'=>'userGroup')),
+				array('label'=>'Membership', 'url'=>array('/membership', 'view'=>'membership')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,15 +6,24 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<h3>Membership management application using PHP Yii Framework and MySql.</h3>
+<p>
+You can add user and assign them role which is called as user group.<br>
+Add multiple memberships with validity days with cost.
+</p>
+<b>First you need to download or clone Yii framework.</b>
+<br>
+Download - http://www.yiiframework.com/download/ OR
+<pre><code>git clone https://github.com/yiisoft/yii.git</code></pre>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+<b>Then download or clone iMembership application.</b>
+<br>
+Download - http://kunalkamble.github.io/iMembership/ OR
+<pre><code>git clone https://github.com/kunalkamble/iMembership.git</code></pre>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<b>Run migration</b>
+<br>
+Go inside protected folder of your applicationa and run following command
+<br>
+<pre><code>./yiic migrate</code></pre>
+( Considering Yii framework and application both are on root directory. )
